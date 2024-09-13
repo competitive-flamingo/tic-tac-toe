75b3baab5e20b36f200d985e7c246131c53b5bb6
Marwan Esam <competitiveflamingo@gmail.com>
2024-09-13 07:43:29 +0300

    Complete GUI and added DOM functionality

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# tic-tac-toe
+# Tic-Tac-Toe
+
+A simple Tic-Tac-Toe game built using HTML, CSS, and JavaScript. The game can be played between two players (X and O) and keeps track of scores as well as tie counts. 
+
+## Features
+
+- Interactive 3x3 Tic-Tac-Toe grid.
+- Two-player functionality.
+- Score tracking for both players and ties.
+- Responsive design.
+
+## Technologies
+
+- HTML5
+- CSS3 (with grid layout for responsive game board)
+- Vanilla JavaScript (for game logic)
--- script.js
+++ script.js
@@ -1,29 +1,87 @@
-const gameboard = (function() {
+const gameboard = (function () {
     let board = ["", "", "", "", "", "", "", "", ""];
+    const domBoardCells = document.querySelectorAll(".cell");
+    const gameContainer = document.querySelector(".game-container");
+    const winConditions = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],    //rows
+        [0, 3, 6], [1, 4 ,7], [2, 5, 8],    //columns
+        [2, 4, 6], [0, 4, 8]                //diagonals
+    ];
     const getBoard = () => board;
+    const getWinConditions = () => winConditions;
     const makeMove = (index, playerMarker) => {
-        if(board[index] === "") {
+        if(board[index] == "") {
             board[index] = playerMarker;
             return true;
         }
         return false;
-    };
+    }
+    const handleCellClick = gameContainer.addEventListener("click", (event) => {
+        const cellIndex = +event.target.getAttribute("index");
+        gameflow.playRound(cellIndex);
+    });
     const resetBoard = () => {
         board = board.map(() => "");
-    };
+    }
+
+    const renderBoard = () => {
+        for(let i = 0 ; i < board.length ; i++) {
+            if(board[i] !== "") {
+                const playerMarkerIcon = document.createElement("img");
+                playerMarkerIcon.src = `./images/${board[i].toLowerCase()}-icon.svg`;
+                playerMarkerIcon.draggable = false;
+                if(domBoardCells[i].childNodes.length) {
+                    domBoardCells[i].replaceChild(playerMarkerIcon, domBoardCells[i].firstChild);
+                }
+                else {
+                    domBoardCells[i].appendChild(playerMarkerIcon);
+                }
+            }
+            else {
+                if(domBoardCells[i].childNodes.length) {
+                    domBoardCells[i].removeChild(domBoardCells[i].firstChild);
+                }
+            }
+        }
+    } 
+
     return {
         getBoard,
+        getWinConditions,
         makeMove,
         resetBoard,
+        renderBoard
     };
 })();
 
-
-function player(marker) {
+const player = function(marker) {
+    let score = 0;
+    const domScoreContainers = document.querySelectorAll(".scores > .p");
     const getMarker = () => marker;
+    const getScore = () => score;
+    const increaseScore = () => {score++};
+    const renderScore = () => {
+        switch(getMarker()) {
+            case "X": 
+                domScoreContainers[0].children[1].textContent = getScore();
+                break;
+            case "O":
+                domScoreContainers[1].children[1].textContent = getScore();
+                break;
+        }
+    }
+    const toggleTurnBorder = () => {
+        for(container of domScoreContainers) {
+            container.children[0].classList.toggle("turn");
+        }
+    }
     return {
         getMarker,
-    }
+        getScore,
+        increaseScore,
+        renderScore,
+        toggleTurnBorder
+    };
 }
 
 const player1 = player("X");
@@ -31,63 +89,55 @@ const player2 = player("O");
 
 const gameflow = (function() {
     let currentPlayer = player1;
+    const domTieScore = document.querySelector(".tie .score");
     const switchPlayer = () => {
-        currentPlayer = currentPlayer === player1? player2 : player1;
+        currentPlayer = currentPlayer === player1 ?
+        player2 : player1;
     };
+    const renderTieScore = () => {
+        domTieScore.textContent = +domTieScore.textContent + 1;
+    }
     const checkWin = () => {
         const board = gameboard.getBoard();
-        const winConditions = [
-            [0, 1, 2], [3, 4, 5], [6, 7, 8],    //rows
-            [0, 3, 6], [1, 4, 7], [2, 5, 8],    //columns
-            [0, 4, 8], [2, 4, 6]                //diagonals
-        ];
-
-        return winConditions.some(condition => {
-            return board[condition[0]] !== "" 
-            && board[condition[0]] === board[condition[1]] 
+        return gameboard.getWinConditions().some(condition => {
+            return board[condition[0]] !== "" &&
+            board[condition[0]] === board[condition[1]] 
             && board[condition[1]] === board[condition[2]];
         });
     };
-
-
-    const checkTie = () => {
+    const checkDraw = () => {
         const board = gameboard.getBoard();
-        return board.every(cell => cell !== "");
-    }
+        return gameboard.getWinConditions().every(condition => {
+            return board[condition[0]] !== "" 
+            && board[condition[1]] !== "" 
+            && board[condition[2]] !== "" 
+            && (board[condition[0]] !== board[condition[1]] || board[condition[0]] !== board[condition[2]]);
+        });
+    };
 
     const playRound = (index) => {
         if(gameboard.makeMove(index, currentPlayer.getMarker())) {
+            currentPlayer.toggleTurnBorder();
+            gameboard.renderBoard();
             if(checkWin()) {
-                console.log(`${currentPlayer.getMarker()} wins!`);
+                currentPlayer.increaseScore();
+                currentPlayer.renderScore();
                 gameboard.resetBoard();
-                currentPlayer = player1; 
+                gameboard.renderBoard();
+                switchPlayer();
             }
-            else if(checkTie()) {
-                console.log("DRAW!");
+            else if(checkDraw()) {
                 gameboard.resetBoard();
-                currentPlayer = player1;
+                gameboard.renderBoard();
+                renderTieScore();
+                switchPlayer();
             }
             else {
                 switchPlayer();
             }
         }
-        else {
-            console.log("Cell already taken. Try again!");
-        }
     }
     return {
         playRound
-    };
+    }
 })();
-
-gameflow.playRound(0);
-gameflow.playRound(1);
-gameflow.playRound(2);
-gameflow.playRound(4);
-gameflow.playRound(3);
-gameflow.playRound(5);
-gameflow.playRound(7);
-gameflow.playRound(6);
-gameflow.playRound(8);
-
-console.log(gameboard.getBoard());
